5b5dc2e558f8b6efbdb825dc5543d95681b97603
Vinicios Gois <viniciosgois@Vinicioss-MacBook-Pro.local>
2021-09-14 18:18:02 -0300

    Challenge Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const id = Math.floor(Math.random() * 1000) + 1;
+    if( newTaskTitle.length !== 0 ) {
+      const task = {
+        id: id,
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks( [...tasks, task]);
+      setNewTaskTitle('');
+      
+    } else {
+        
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, isComplete : !task.isComplete } : task
+      )
+    )
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter((task) => task.id !== id));
   }
 
   return (
